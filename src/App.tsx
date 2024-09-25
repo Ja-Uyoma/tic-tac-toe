0a6e3ff9075ee5ece34cc9117f2df54551c5c805
Jimmy Givans <jauyoma.dev@gmail.com>
2024-09-25 16:59:36 +0300

    feat: add function getWinner
    - This function scans the grid for winning combinations and returns
      the winner if there is one

--- src/App.tsx
+++ src/App.tsx
@@ -88,4 +88,23 @@ function App() {
   );
 }
 
+function getWinner(grid: string[]): string | null {
+  const winningCombinations = [
+    [0, 4, 8],
+    [2, 4, 6],
+    [1, 4, 7],
+    [3, 4, 5],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+
+    if (grid[a] === grid[b] && grid[b] === grid[c]) {
+      return grid[a];
+    }
+  }
+
+  return null;
+}
+
 export default App;
