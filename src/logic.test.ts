465a17d7ca37a1d3aaa644a518dfb59bdc7cfdbc
Jimmy Givans <jauyoma.dev@gmail.com>
2024-09-27 16:50:02 +0300

    test: pass failing tests by updating the type of the grid variables

--- src/logic.test.ts
+++ src/logic.test.ts
@@ -1,21 +1,21 @@
 import { describe, expect, it } from "vitest";
-import { getWinner } from "./logic";
+import { CrossOrNought, getWinner } from "./logic";
 
 describe("getWinner", () => {
   it("gets the right winner if there is one", () => {
-    const grid = ["X", "O", "O", "X", "X", "O", "O", "X", "X"];
+    const grid: CrossOrNought[] = ["X", "O", "O", "X", "X", "O", "O", "X", "X"];
     const winner = getWinner(grid);
     expect(winner).toBe("X");
   });
 
   it("returns null if there is no winner", () => {
-    const grid = ["X", "O", "X", "X", "O", "O", "O", "X", "X"];
+    const grid: CrossOrNought[] = ["X", "O", "X", "X", "O", "O", "O", "X", "X"];
     const winner = getWinner(grid);
     expect(winner).toBe(null);
   });
 
   it("returns null if the grid is empty", () => {
-    const grid: string[] = [];
+    const grid: CrossOrNought[] = [];
     const winner = getWinner(grid);
     expect(winner).toBe(null);
   });
