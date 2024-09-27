e70862fb28a869287ed4ad062de8692262facb29
Jimmy Givans <jauyoma.dev@gmail.com>
2024-09-27 16:21:50 +0300

    test: add tests for getWinner

---
+++ src/logic.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from "vitest";
+import { getWinner } from "./logic";
+
+describe("getWinner", () => {
+  it("gets the right winner if there is one", () => {
+    const grid = ["X", "O", "O", "X", "X", "O", "O", "X", "X"];
+    const winner = getWinner(grid);
+    expect(winner).toBe("X");
+  });
+
+  it("returns null if there is no winner", () => {
+    const grid = new Array(9).fill(" ");
+    const winner = getWinner(grid);
+    expect(winner).toBe(null);
+  });
+});
